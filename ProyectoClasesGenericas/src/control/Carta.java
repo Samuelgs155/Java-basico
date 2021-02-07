2a806969476602a5b85c10d45aaf3cafe86d2c5a
Samuelgs155 <samuelgs15@hotmail.com>
2021-02-07 12:39:46 +0100

    Add files via upload

---
+++ ProyectoClasesGenericas/src/control/Carta.java
@@ -0,0 +1,16 @@
+package control;
+
+public class Carta {
+
+	private int numero;
+    private String palo;
+
+    Carta(int numero, String palo) {
+        this.numero = numero;
+        this.palo = palo;
+    }
+
+    public void imprimir() {
+        System.out.println(numero + " " + palo);
+    }
+}
---
+++ ProyectoClasesGenericas/src/control/Persona.java
@@ -0,0 +1,16 @@
+package control;
+
+public class Persona {
+	private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void imprimir() {
+        System.out.println(nombre + " " + edad);
+    }
+
+}
